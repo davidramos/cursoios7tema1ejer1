c1893d5eeb3cd30ff101e7afd59ecdef6d07fcab
Collection design <developer@collectiondesign.es>
2014-03-05 00:23:25 +0100

    Añadida nueva clase.

---
+++ cursoios7tema1ejer1/NewClass.h
@@ -0,0 +1,13 @@
+//
+//  NewClass.h
+//  cursoios7tema1ejer1
+//
+//  Created by David on 05/03/14.
+//  Copyright (c) 2014 David Ramos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewClass : NSObject
+
+@end
---
+++ cursoios7tema1ejer1/NewClass.m
@@ -0,0 +1,13 @@
+//
+//  NewClass.m
+//  cursoios7tema1ejer1
+//
+//  Created by David on 05/03/14.
+//  Copyright (c) 2014 David Ramos. All rights reserved.
+//
+
+#import "NewClass.h"
+
+@implementation NewClass
+
+@end
